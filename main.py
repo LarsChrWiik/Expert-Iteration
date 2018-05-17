bb7231b7241058b978441661206fa6618a15d950
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-05-17 17:13:33 +0100

    Tested GameHandler and random player.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
 from Games.TicTacToe import TicTacToe
+from Games.GameHandler import GameHandler
+from Players.RandomPlayer import RandomPlayer
 
 
 def main():
+
+    handler = GameHandler(
+        game=TicTacToe(),
+        players=[RandomPlayer(), RandomPlayer()]
+    )
+
+    handler.start_game()
+
+
+    """
     ttt1 = TicTacToe()
     ttt1.advance(player_index=1, action_index=4)
     ttt = ttt1.get_state_copy()
@@ -14,6 +26,8 @@ def main():
     ttt.advance(player_index=1, action_index=6)
     ttt.display()
     print(ttt.winner)
+    """
+
 
 
 if __name__ == "__main__":
