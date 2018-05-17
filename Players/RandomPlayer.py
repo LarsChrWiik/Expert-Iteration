5c82de33b7dbb86d9fdce6e2ec4cf2a0e9e3786d
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-05-17 17:11:57 +0100

    Added a player that plays random moves.

---
+++ Players/RandomPlayer.py
@@ -0,0 +1,15 @@
+
+from Players.BasePlayer import BasePlayer
+from Games.BaseGame import BaseGame
+import random
+
+
+class RandomPlayer(BasePlayer):
+    """
+    Player that plays random moves.
+    """
+
+    def make_move(self, game: BaseGame):
+        legal_moves = game.get_legal_moves(self.player_index)
+        action_index = random.choice(legal_moves)
+        game.advance(player_index=self.player_index, action_index=action_index)
