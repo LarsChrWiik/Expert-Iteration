6a36d4792f4b8c5499ca2081890aa9c9d9497c46
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-05-20 22:34:31 +0100

    Added MLP class as a Policy and value predictor. Not complete implementaiton.

---
+++ ExIt/PolicyValuePredictors/MLP.py
@@ -0,0 +1,12 @@
+
+from ExIt.PolicyValuePredictors.BasePolicyValuePredictor import BasePolicyValuePredictor
+
+
+class MLP(BasePolicyValuePredictor):
+    """
+    Multi Layer Perceptron implementation.
+    """
+
+    def pred_value(self, feature_vector):
+        # TODO: Fix.
+        return 0.1
