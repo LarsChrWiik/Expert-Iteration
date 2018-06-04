2cf38c3e0301e579f22b3670c697798f96b5756e
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-05 00:28:26 +0100

    Cleanup.

--- Players/MinimaxMlpPlayer.py
+++ Players/MinimaxMlpPlayer.py
@@ -1,21 +1,13 @@
 
-from Players.BasePlayers.BaseExItPlayer import BaseExItPlayer
+from Players.Base import BaseExItPlayer
 from ExIt.Apprentice.MLP import MLP
 from ExIt.Expert.MiniMax import MiniMax
 from ExIt.ExpertIteration import ExpertIteration
 
 
 class MinimaxMlpPlayer(BaseExItPlayer):
-    """
-    Player that uses:
-        - Minimax as Policy improvement operator.
-        - NN as Policy and evaluation predictor.
-    """
+    """ Player that uses Minimax as expert and NN as apprentice """
 
     def __init__(self, minimax_depth):
-        super().__init__(
-            exIt_algorithm=ExpertIteration(
-                apprentice=MLP(),
-                expert=MiniMax(depth=minimax_depth)
-            )
-        )
+        super().__init__(ex_it_algorithm=ExpertIteration(
+            apprentice=MLP(), expert=MiniMax(depth=minimax_depth)))
