e03fd7a61f4e23ef90ca84fd808e4bd61deba621
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-04 01:34:48 +0100

    Cleanup.

--- Players/RandomPlayer.py
+++ Players/RandomPlayer.py
@@ -1,15 +1,13 @@
 
-from Players.BasePlayers.BasePlayer import BasePlayer
+from Players.Base import BasePlayer
 from Games.BaseGame import BaseGame
 import random
 
 
 class RandomPlayer(BasePlayer):
-    """
-    Player that plays random moves.
-    """
+    """ Player that plays random moves """
 
-    def make_expert_move(self, game: BaseGame):
+    def move(self, game: BaseGame):
         legal_moves = game.get_legal_moves(self.player_index)
         action_index = random.choice(legal_moves)
         game.advance(action_index=action_index)
