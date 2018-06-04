36b632c7ba2fd1a1c283d7253b9d22d027d93526
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-05 00:13:11 +0100

    Cleanup.

--- Players/NnMctsPlayer.py
+++ Players/NnMctsPlayer.py
@@ -1,19 +1,12 @@
 
-from Players.BasePlayers.BaseExItPlayer import BaseExItPlayer
+from Players.Base import BaseExItPlayer
 from ExIt.Apprentice.MLP import MLP
 from ExIt.Expert.MCTS import MCTS
 from ExIt.ExpertIteration import ExpertIteration
 
 
 class NnMctsPlayer(BaseExItPlayer):
-    """
-    Player that uses:
-        - MCTS as Policy improvement operator.
-        - NN as Policy and evaluation predictor.
-    """
+    """Player that uses MCTS as the expert and NN as the apprentice """
 
     def __init__(self):
-        super().__init__(exIt_algorithm=ExpertIteration(
-            policy_value_predictor=MLP(),
-            policy_improvement_operator=MCTS()
-        ))
+        super().__init__(ex_it_algorithm=ExpertIteration(apprentice=MLP(), expert=MCTS()))
