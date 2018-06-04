11ae7d2c1835d0593490c86fef369798a8627fb1
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-05 00:24:18 +0100

    Added comments and cleanup.

--- ExIt/ExpertIteration.py
+++ ExIt/ExpertIteration.py
@@ -8,42 +8,25 @@ from random import choice as rnd_choice
 
 
 class ExpertIteration:
-    """
-    Expert Iteration.
-    """
-
-    # NN.
-    apprentice = None
-
-    # MCTS or AB-search.
-    expert = None
-
-    data_set = DataSet()
-
-    game_class = None
 
     def __init__(self, apprentice: BaseApprentice, expert: BaseExpert):
         self.apprentice = apprentice
         self.expert = expert
+        self.data_set = DataSet()
 
     def calculate_best_action(self, state: BaseGame):
-        """
-        This is used after the Expert Iteration has completed.
-
-        :return: int, action index.
-        """
         return self.expert.start(state=state, predictor=self.apprentice)
 
-    def start_exIt(self, game_class, num_iteration, randomness: bool):
+    def start_ex_it(self, game_class, num_iteration, add_randomness: bool):
         """
         Start Expert Iteration to master the given game.
-        This process might take some time.
+        This process is time consuming.
         """
         self.apprentice.init_model(input_fv_size=game_class().fv_size,
                                    pi_size=game_class().num_actions)
-        self.game_class = game_class
 
         for i in range(num_iteration):
+            # TODO: Move to NN maybe?
             if i == int(num_iteration/4):
                 self.apprentice.set_lr(new_lr=0.1)
             elif i == int(num_iteration/2):
@@ -53,21 +36,23 @@ class ExpertIteration:
 
             state = game_class()
 
-            # TODO: fix.
+            # TODO: Ask Spyros.
             x = (1 - (i / num_iteration))
-            #t = 0 if not randomness else x
-            t = 0 if not randomness else 0.1
-            # t = 0.0 if not randomness else 1.0 / state.num_actions
-            #t = 0 if not randomness else log(1+x)
-            #t = 0 if not randomness else 4**(-2*(i / num_iteration))
+            #rnd_prob = 0 if not randomness else x
+            rnd_prob = 0 if not add_randomness else 0.1
+            # rnd_prob = 0.0 if not randomness else 1.0 / state.num_actions
+            #rnd_prob = 0 if not randomness else log(1+x)
+            #rnd_prob = 0 if not randomness else 4**(-2*(i / num_iteration))
 
             X = state.get_feature_vector(state.turn)
             while not state.has_finished():
-                did_random = self.exIt_state(state=state, randomness=t)
+                # TODO: Maybe remove return.
+                did_random = self.ex_it_state(state=state, rnd_prob=rnd_prob)
+                # TODO: Maybe remove.
                 #if i > 500 and did_random:
-                 #   t = t / 2
+                 #   rnd_prob = rnd_prob / 2
             print("*** Iteration = " + str(i) + " ***")
-            print("randomness = " + str(t))
+            print("randomness = " + str(rnd_prob))
             print("    r = ", self.data_set.r_array)
             # TODO: Check this.
             self.data_set.update_reward_hard(state)
@@ -84,8 +69,7 @@ class ExpertIteration:
             print("")
             print("")
 
-    # TODO: change name of randomness. = t.
-    def exIt_state(self, state: BaseGame, randomness: float):
+    def ex_it_state(self, state: BaseGame, rnd_prob: float):
 
         # Ask expert for the best move.
         action_index, r, pi_update = self.expert.start(state=state, predictor=self.apprentice)
@@ -93,11 +77,12 @@ class ExpertIteration:
         self.data_set.add_sample(state=state, pi_update=pi_update, r=r)
 
         did_random = False
-        if uniform(0, 1) < randomness:
+        if uniform(0, 1) < rnd_prob:
             did_random = True
             a = state.get_legal_moves(state.turn)
             action_index = rnd_choice(a)
 
+            # TODO: Fix later.
             """
             if uniform(0, 1) > randomness:
                 # Random move based on action probabilities.
@@ -119,4 +104,5 @@ class ExpertIteration:
 
         state.advance(action_index=action_index)
 
+        # TODO: Maybe remove.
         return did_random
