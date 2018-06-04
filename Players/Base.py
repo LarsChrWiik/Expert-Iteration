bd72d000d4e714a957e9d6f6fb12a79e01909d53
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-04 01:28:53 +0100

    Rename and cleanup.

---
+++ Players/Base.py
@@ -0,0 +1,44 @@
+
+
+from ExIt.ExpertIteration import ExpertIteration
+from Games.BaseGame import BaseGame
+
+
+class BasePlayer:
+    """ Base player that is able to move """
+
+    def __init__(self):
+        self.player_index = None
+
+    def move(self, state: BaseGame):
+        raise NotImplementedError("Please Implement this method")
+
+
+class BaseExItPlayer(BasePlayer):
+    """ Base player that is able to improve its strategy using Expert Iteration """
+
+    def __init__(self, ex_it_algorithm: ExpertIteration):
+        super(BasePlayer, self).__init__()
+        self.__ex_it_algorithm = ex_it_algorithm
+
+    def start_ex_it(self, game_class, num_iteration, randomness: bool):
+        """ Starts Expert Iteration. NB: Time consuming process """
+        self.__ex_it_algorithm.start_ex_it(game_class=game_class, num_iteration=num_iteration,
+                                           randomness=randomness)
+
+    def move(self, state: BaseGame):
+        """ Calculate the best move """
+        action_index, evaluation, pi_update = self.__ex_it_algorithm.calculate_best_action(state)
+
+        # TODO: Remove later (Used for testing).
+        print("fv = ", state.get_feature_vector(state.turn))
+        print("evaluation = ", self.__ex_it_algorithm.apprentice.pred_eval(
+            X=state.get_feature_vector(state.turn)))
+        print("action prob = ", self.__ex_it_algorithm.apprentice.pred_prob(
+            X=state.get_feature_vector(state.turn)))
+        print("turn = " + str(state.turn))
+        print("action_index = " + str(action_index))
+        print("policy improver eval = " + str(evaluation))
+        print("updated pi_update = " + str(pi_update))
+
+        state.advance(action_index=action_index)
