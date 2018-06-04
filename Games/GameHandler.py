0cc119cf604a676c5309e686406adf32aab48848
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-04 01:33:50 +0100

    Cleanup.

--- Games/GameHandler.py
+++ Games/GameHandler.py
@@ -1,32 +1,23 @@
 
 from Games.BaseGame import BaseGame
-from Players.BasePlayers.BasePlayer import BasePlayer
+from Players.Base import BasePlayer
 
 
 class GameHandler:
-    """
-    Class object to organize a game between players.
-    """
-
-    game = None
-    players = []
+    """ Class to organize games between players """
 
     def __init__(self, game: BaseGame, players: [BasePlayer]):
-        """
-        :param game: BaseGame object.
-        :param players: List of BasePlayers, the order of the players is the turn order.
-        """
         self.game = game
         self.players = players
 
-        # Set indexes for all players in the game.
+        # Index all players in the game.
         for index, p in enumerate(players):
             p.player_index = index
 
     def start_game(self):
-        """
-        Starts the game and let the player play until the game has finished.
-        """
+        """ Starts the game and let the players play until the game has finished """
         while not self.game.has_finished():
             self.players[self.game.turn].make_expert_move(self.game)
+
+            # TODO: Remove later. 
             self.game.display()
