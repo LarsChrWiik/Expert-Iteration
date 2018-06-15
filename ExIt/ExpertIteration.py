6f85f71ad65ab0c5f89262daa7bdd981fbf9702c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-15 19:59:10 +0100

    Major refactoring and cleanup. Moves can now be made using apprentice only. ExIt still uses apprentice and expert.

--- ExIt/ExpertIteration.py
+++ ExIt/ExpertIteration.py
@@ -10,65 +10,73 @@ import numpy as np
 
 class ExpertIteration:
 
+    @staticmethod
+    def get_rnd_prob(iteration, num_iteration):
+        return 1 - (iteration / num_iteration)
+
     def __init__(self, apprentice: BaseApprentice, expert: BaseExpert):
         self.apprentice = apprentice
         self.expert = expert
         self.data_set = DataSet()
 
-    def calculate_best_action(self, state: BaseGame):
-        return self.expert.start(state=state, predictor=self.apprentice)
+    def apprentice_output(self, state: BaseGame):
+        """ Evaluate state and calculates the best action index
+            using only the apprentice """
+        fv = state.get_feature_vector(state.turn)
+        action_indexes = self.apprentice.pred_prob(fv)
+        # Remove illegal moves.
+        legal_moves = state.get_possible_actions()
+        for i, v in enumerate(action_indexes):
+            if i not in legal_moves:
+                action_indexes[i] = 0
+        # Choose best action.
+        action_index = np.argmax(action_indexes)
+        evaluation = self.apprentice.pred_eval(fv)
+        return action_index, evaluation
 
-    def start_ex_it(self, game_class, num_iteration, add_randomness: bool):
+    def start_ex_it(self, game_class, num_iteration, add_randomness: bool, search_time: float):
         """ Start Expert Iteration to master the given game.
-            This process is time consuming """
+            This process is time consuming. """
 
-        self.apprentice.init_model(input_fv_size=game_class().fv_size,
-                                   pi_size=game_class().num_actions)
-
-        #changes = [y * 0.1 for y in range(1, 11)]
-        #lr_change = [int(num_iteration) * x for x in changes]
+        rnd_prob = 0.0
         for i in range(num_iteration):
-            print("lr = ", float(self.apprentice.get_lr()))
-            # TODO: Move to NN maybe?
-            #if i in lr_change:
-                #self.apprentice.set_lr(new_lr=self.apprentice.get_lr()*0.9)
-
             state = game_class()
-
-            max_rnd = 1.0
-            rnd_prob = 0 if not add_randomness else (1 - (i / num_iteration)) * max_rnd
-
             X = state.get_feature_vector(state.turn)
-            while not state.has_finished():
-                self.ex_it_state(state=state, rnd_prob=rnd_prob)
+
+            if add_randomness:
+                rnd_prob = ExpertIteration.get_rnd_prob(iteration=i, num_iteration=num_iteration)
+
+            while not state.is_game_over():
+                self.ex_it_state(state=state, rnd_prob=rnd_prob, search_time=search_time)
+
+            # TODO: Remove print info later.
             print("*** Iteration = " + str(i) + " ***")
             print("randomness = " + str(rnd_prob))
-            print("    r = ", self.data_set.r_array)
-            # TODO: Check this.
+            print("     r = ", self.data_set.r_array)
             self.data_set.update_reward_hard(state)
-            print("r_new = ", self.data_set.r_array)
-
-            # Train the Policy and Evaluation Predictor.
+            print(" r_new = ", self.data_set.r_array)
             s_array, pi_array, r_array = self.data_set.extract_data()
-            if len(s_array) > 0:
-                print("    pi = ", self.apprentice.pred_prob(X=X))
-                print("pi_new = ", pi_array[0])
-                print("r_start= ", self.apprentice.pred_eval(X=X))
-                print("")
-                self.apprentice.train(X=s_array, Y_pi=pi_array, Y_r=r_array)
-            print("")
-            print("")
+            print("    pi = ", self.apprentice.pred_prob(X=X))
+            print("pi_new = ", pi_array[0])
+            print("r_start= ", self.apprentice.pred_eval(X=X))
+            print(""); print(""); print("")
 
-    def ex_it_state(self, state: BaseGame, rnd_prob: float):
+            self.apprentice.train(X=s_array, Y_pi=pi_array, Y_r=r_array)
+
+    def ex_it_state(self, state: BaseGame, rnd_prob: float, search_time: float):
         """ Expert Iteration for a given state """
-        action_index, r = self.expert.start(state=state, predictor=self.apprentice)
+        action_index, r = self.expert.search(state=state,
+                                             predictor=self.apprentice,
+                                             search_time=search_time)
         if uniform(0, 1) < rnd_prob:
-            action_index = rnd_choice(state.get_legal_moves(state.turn))
-
-        # Create action probability.
-        pi_update = np.zeros(state.num_actions, dtype=float)
-        pi_update[action_index] = 1
-
-        self.data_set.add_sample(state=state, pi_update=pi_update, r=r)
-
+            # Random move.
+            action_index = rnd_choice(state.get_possible_actions())
+            r = self.get_reward_for_action(state=state, action_index=action_index)
+        self.data_set.add_sample(state=state, action_index=action_index, r=r)
         state.advance(action_index=action_index)
+
+    def get_reward_for_action(self, state: BaseGame, action_index: int):
+        """ Calculates the reward for the given action index """
+        c = state.copy()
+        c.advance(action_index)
+        return self.apprentice.pred_eval(X=c.get_feature_vector(state.turn))
