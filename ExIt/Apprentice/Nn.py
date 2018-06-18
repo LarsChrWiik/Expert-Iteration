6bf6eee7a1a2d8b5db949b6775fbc2ee9b4218cf
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-18 16:28:58 +0100

    Modified parameters and added dropout.

--- ExIt/Apprentice/Nn.py
+++ ExIt/Apprentice/Nn.py
@@ -2,12 +2,13 @@
 from ExIt.Apprentice.BaseApprentice import BaseApprentice
 from keras.optimizers import SGD, Adam
 from keras.layers.core import Dense
+from keras.layers.normalization import BatchNormalization
 from keras.models import Model
-from keras.layers import Input, LeakyReLU
+from keras.layers import Input, Activation, Dropout
 import numpy as np
 import keras.backend as K
-from keras.losses import mean_squared_error, categorical_crossentropy as cc
-# from keras.regularizers import l2
+from keras.losses import mean_squared_error, categorical_crossentropy
+from keras.regularizers import l2
 
 
 def entropy(y):
@@ -16,46 +17,59 @@ def entropy(y):
     return -K.sum(y * K.log(y))
 
 
-def custom_loss(y_true, y_pred, beta=0.001):
-    return cc(y_true, y_pred) + beta * entropy(y_pred)
+def custom_loss(y_true, y_pred, beta=0.1):
+    return categorical_crossentropy(y_true, y_pred) - beta * entropy(y_pred)
 
 
 class Nn(BaseApprentice):
-    """ Multi Layer Perceptron (MLP) implementation """
+    """ Deep neural network implementation (Apprentice) """
 
-    model = None
-    optimizer = None
-    eval_size = 1
+    def __init__(self):
+        self.model = None
+        self.optimizer = None
 
     def init_model(self, input_fv_size, pi_size):
+        # TODO: Implement Batch normalization?.
+        # Input layer.
+        x = Input(shape=(input_fv_size,))
 
-        x = Input(shape=(input_fv_size,))                                   # Input layer
-        h1 = Dense(32)(x)                                                   # Hidden layer 1
-        h1 = LeakyReLU()(h1)                                                # Hidden layer 1 (Act)
-        h2 = Dense(32)(h1)                                                  # Hidden layer 2
-        h2 = LeakyReLU()(h2)                                                # Hidden layer 2 (Act)
-        pi = Dense(pi_size, activation='softmax', name='pi_output')(h2)     # Output layer 1
-        v = Dense(self.eval_size, activation='linear', name='r_output')(h2) # Output layer 2
+        # Hidden layers.
+        h1 = Dense(32, kernel_regularizer=l2())(x)
+        h1 = Activation("elu")(h1)
+        h1 = Dropout(rate=0.2)(h1)
+        h2 = Dense(32, kernel_regularizer=l2())(h1)
+        h2 = Activation("elu")(h2)
+        h2 = Dropout(rate=0.2)(h2)
 
-        self.model = Model(inputs=[x], outputs=[pi, v])
-        self.model.summary()
+        # Output layers.
+        pi = Dense(pi_size, activation='softmax', name='pi_output', kernel_regularizer=l2())(h2)
+        v = Dense(1, activation='linear', name='r_output', kernel_regularizer=l2())(h2)
 
-        # TODO: Maybe remove later.
+        model = Model(inputs=[x], outputs=[pi, v])
+        model.summary()
+
+        # TODO: Maybe remove.
+        """
         loss_weights_pi = pi_size/(pi_size+1)
         loss_weights_r = 1/(pi_size+1)
         loss_weights = {
             'pi_output': loss_weights_pi,
             'r_output': loss_weights_r
         }
+        """
 
         optimizer = Adam()
+        model.compile(
+            optimizer=optimizer,
+            loss={
+                'pi_output': custom_loss,
+                'r_output': mean_squared_error
+            },
+            loss_weights=None
+        )
+
         self.optimizer = optimizer
-        self.model.compile(optimizer=self.optimizer,
-                           loss={
-                               'pi_output': custom_loss,
-                               'r_output': mean_squared_error
-                           },
-                           loss_weights=None)
+        self.model = model
 
     def train(self, X, Y_pi, Y_r):
         self.model.fit(x=np.array(X), y=[np.array(Y_pi), np.array(Y_r)])
