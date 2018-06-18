9d8e2d82894335552d10eb09e3f05f1f1a08bc5d
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-18 16:39:01 +0100

    Refactoring of print statements.

--- ExIt/ExpertIteration.py
+++ ExIt/ExpertIteration.py
@@ -1,23 +1,29 @@
 
 from ExIt.Apprentice import BaseApprentice
 from ExIt.Expert import BaseExpert
-from Games.BaseGame import BaseGame
+from Games.GameLogic import BaseGame
 from ExIt.DataSet import DataSet
 from random import uniform
 from random import choice as rnd_choice
+from Support.Timer import Timer
 import numpy as np
+
+
+timer = Timer()
 
 
 class ExpertIteration:
 
     @staticmethod
     def get_rnd_prob(iteration, num_iteration):
-        return 1 - (iteration / num_iteration)
+        return 0.1
+        #return 1 - (iteration / num_iteration)
 
     def __init__(self, apprentice: BaseApprentice, expert: BaseExpert):
         self.apprentice = apprentice
         self.expert = expert
         self.data_set = DataSet()
+        self.games_played = 0
 
     def apprentice_output(self, state: BaseGame):
         """ Evaluate state and calculates the best action index
@@ -40,28 +46,31 @@ class ExpertIteration:
 
         rnd_prob = 0.0
         for i in range(num_iteration):
+
             state = game_class()
             X = state.get_feature_vector(state.turn)
 
+            # TODO: Change later.
             if add_randomness:
                 rnd_prob = ExpertIteration.get_rnd_prob(iteration=i, num_iteration=num_iteration)
 
             while not state.is_game_over():
                 self.ex_it_state(state=state, rnd_prob=rnd_prob, search_time=search_time)
-
-            # TODO: Remove print info later.
-            print("*** Iteration = " + str(i) + " ***")
-            print("randomness = " + str(rnd_prob))
-            print("     r = ", self.data_set.r_array)
-            self.data_set.update_reward_hard(state)
-            print(" r_new = ", self.data_set.r_array)
+            self.games_played += 1
+            #self.data_set.update_reward_hard(state)
             s_array, pi_array, r_array = self.data_set.extract_data()
-            print("    pi = ", self.apprentice.pred_prob(X=X))
-            print("pi_new = ", pi_array[0])
-            print("r_start= ", self.apprentice.pred_eval(X=X))
-            print(""); print(""); print("")
-
             self.apprentice.train(X=s_array, Y_pi=pi_array, Y_r=r_array)
+
+            # TODO: Delete later.
+            print("*** Iteration = " + str(self.games_played) + " ***")
+            print("randomness = " + str(rnd_prob))
+            print("r_array = ", self.data_set.r_array)
+            print("     pi = ", self.apprentice.pred_prob(X=X))
+            print(" pi_new = ", pi_array[0])
+            print("r_start = ", self.apprentice.pred_eval(X=X))
+            print("")
+            print("")
+            print("")
 
     def ex_it_state(self, state: BaseGame, rnd_prob: float, search_time: float):
         """ Expert Iteration for a given state """
