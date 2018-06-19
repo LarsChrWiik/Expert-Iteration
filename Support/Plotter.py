760489e94f48a92dfa239324c034f2cfb80aaa79
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-19 22:10:21 +0100

    Cleanup and stores to disk.

--- Support/Plotter.py
+++ Support/Plotter.py
@@ -1,19 +1,20 @@
 
 import matplotlib.pyplot as plt
-
-# TODO: Fix metafile.
 import csv
-def plot_test(metafile=None, file=None):
 
-    with open('./Statistics/2018-06-18___02-20-59/0.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
+
+def plot_result(folder, filename):
+    base_path = './Statistics' + "/" + folder
+    path = base_path + "/" + filename + ".csv"
+    with open(path) as csv_file:
+        reader = csv.DictReader(csv_file)
         data = [[float(row["win"]), float(row["loss"]), float(row["draw"])] for row in reader]
 
         print(data)
-        iterations = 1000
-        wins, loses, draws = get_statistics(data=data, iterations=iterations)
+        iterations = __get_iterations(base_path=base_path)
+        wins, losses, draws = __get_statistics(data=data, iterations=iterations)
         plt.plot(wins, label="Wins")
-        plt.plot(loses, label="Loses")
+        plt.plot(losses, label="Losses")
         plt.plot(draws, label="Draws")
         plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
                    ncol=3, mode="expand", borderaxespad=0.)
@@ -21,12 +22,19 @@ def plot_test(metafile=None, file=None):
         plt.show()
 
 
-def get_statistics(data, iterations):
+def __get_iterations(base_path):
+    path = base_path + "/" + "metadata" + ".txt"
+    with open(path) as txt_file:
+        content = txt_file.readlines()
+        return float(content[2].split(" ")[-1])
+
+
+def __get_statistics(data, iterations):
     wins = []
-    loses = []
+    losses = []
     draws = []
     for v in data:
         wins.append(v[0] / iterations)
-        loses.append(v[1] / iterations)
+        losses.append(v[1] / iterations)
         draws.append(v[2] / iterations)
-    return wins, loses, draws
+    return wins, losses, draws
