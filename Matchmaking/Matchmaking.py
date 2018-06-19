b692bc7eec5d6d9223d5e3d4d7b9205250aed1a4
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-19 22:08:29 +0100

    Change din the randomness.

--- Matchmaking/Matchmaking.py
+++ Matchmaking/Matchmaking.py
@@ -4,7 +4,7 @@ from Players.Players import BasePlayer, BaseExItPlayer
 from Games.GameLogic import GameResult
 from operator import add
 from random import uniform
-from Players.BasePlayer import assign_game_index
+from Players.BasePlayers import assign_game_index
 from Matchmaking.Statistics import Statistics
 
 
@@ -16,15 +16,12 @@ class GameHandler:
         self.players = players
         assign_game_index(players=players)
 
-    def start_game(self, rnd_prob=0.0):
-        """ Starts the game and let the players play until the game has finished
+    def start_game(self, randomness):
+        """ Starts the game and let the players play until the game has finished.
             If the player is a ExIt player, then only the apprentice is used
             to decide the move. """
         while not self.game.is_game_over():
-            if uniform(0, 1) < rnd_prob:
-                self.players[self.game.turn].move_random(self.game)
-            else:
-                self.players[self.game.turn].move(self.game)
+            self.players[self.game.turn].move(self.game, randomness=randomness)
 
     def get_game_index_result(self):
         """ Return a list of GameResult for each player according to Game Index """
@@ -38,12 +35,12 @@ class Matchmaking:
     """ Logic for matching players """
 
     @staticmethod
-    def match(game_class, players, rnd_prob=0.0):
+    def match(game_class, players, randomness):
         """ Starts a new game match between players and returns the result.
             Return a list of GameResult for each player according to Game Index. """
         game = game_class()
         game_handler = GameHandler(game=game, players=players)
-        game_handler.start_game(rnd_prob=rnd_prob)
+        game_handler.start_game(randomness=randomness)
         game_index_result = game_handler.get_game_index_result()
         result = Matchmaking.get_index_result(
             game_index_result=game_index_result,
@@ -69,7 +66,7 @@ class Matchmaking:
             p.index = index
 
     def compare_ex_it(self, num_train_epoch, search_time,
-                      num_matches, file_name, num_iteration):
+                      num_matches, file_name, num_iteration, randomness):
         """ Compare players through several iterations.
             Self play is enabled for ExIt players between iterations.
             NB: This process never ends if num_iteration = None. """
@@ -92,7 +89,7 @@ class Matchmaking:
             This process accepts non-ExIt player as well such as RandomPlayer. """
         i = 0
         while True:
-            results = self.__compare(num_matches=num_matches)
+            results = self.__compare(num_matches=num_matches, randomness=randomness)
             self.statistics.save(results=results, i=i)
             if num_iteration is not None and i >= num_iteration:
                 break
@@ -106,18 +103,22 @@ class Matchmaking:
             if isinstance(player, BaseExItPlayer):
                 player.start_ex_it(
                     num_iteration=num_train_epoch,
-                    randomness=True,
+                    move_prob_rnd=True,
                     search_time=search_time
                 )
 
-    def __compare(self, num_matches):
+    def __compare(self, num_matches, randomness):
         """ Compare players and store the statistics """
         # 2D list: [win, lose, draw]. Position = player index. (NOT GAME INDEX).
         results = [GameResult.get_new_result_list() for _ in self.players]
 
         for _ in range(num_matches):
             # Play game between players.
-            game_result = Matchmaking.match(game_class=self.game_class, players=self.players)
+            game_result = Matchmaking.match(
+                game_class=self.game_class,
+                players=self.players,
+                randomness=randomness
+            )
             # Get list of results for each player.
             game_result = GameResult.get_players_result_list_(result=game_result)
             # Add the result to the list of total results.
