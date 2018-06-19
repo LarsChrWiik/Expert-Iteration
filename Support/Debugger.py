73733ac7ac201904bbecb906a59d2786929f9ce3
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-19 22:09:43 +0100

    Cleanup and refactoring.

--- Support/Debugger.py
+++ Support/Debugger.py
@@ -1,34 +1,35 @@
 
 from Games.TicTacToe import TicTacToe
+from Players.BasePlayers import BaseExItPlayer
 
 
-class TicTacToeDebugger:
+def debug_display_win_moves(player):
+    debug_display_ttt(player=player, action_list=[0, 4, 1, 8])
+    debug_display_ttt(player=player, action_list=[4, 1, 0, 7])
+    debug_display_ttt(player=player, action_list=[8, 0, 7, 2])
+    debug_display_ttt(player=player, action_list=[8, 0, 7, 5])
+    debug_display_ttt(player=player, action_list=[4, 1, 6, 3])
+    debug_display_ttt(player=player, action_list=[0, 1, 3, 7])
 
-    def debug_display_win_moves(self, player):
-        self.debug_display_test(player=player, action_list=[0, 4, 1, 8])
-        self.debug_display_test(player=player, action_list=[4, 1, 0, 7])
-        self.debug_display_test(player=player, action_list=[8, 0, 7, 2])
-        self.debug_display_test(player=player, action_list=[8, 0, 7, 5])
-        self.debug_display_test(player=player, action_list=[4, 1, 6, 3])
-        self.debug_display_test(player=player, action_list=[0, 1, 3, 7])
 
-    def debug_display(self, player):
-        self.debug_display_test(player=player, action_list=[])
-        self.debug_display_test(player=player, action_list=[4])
-        self.debug_display_test(player=player, action_list=[4, 1])
-        self.debug_display_test(player=player, action_list=[4, 0])
-        self.debug_display_test(player=player, action_list=[1, 4, 3])
-        self.debug_display_test(player=player, action_list=[1, 4, 3, 5])
-        self.debug_display_test(player=player, action_list=[1, 4, 5, 3])
-        self.debug_display_test(player=player, action_list=[4, 0, 8, 5])
+def debug_display_general_moves(player):
+    debug_display_ttt(player=player, action_list=[])
+    debug_display_ttt(player=player, action_list=[4])
+    debug_display_ttt(player=player, action_list=[4, 1])
+    debug_display_ttt(player=player, action_list=[4, 0])
+    debug_display_ttt(player=player, action_list=[1, 4, 3])
+    debug_display_ttt(player=player, action_list=[1, 4, 3, 5])
+    debug_display_ttt(player=player, action_list=[1, 4, 5, 3])
+    debug_display_ttt(player=player, action_list=[4, 0, 8, 5])
 
-    @staticmethod
-    def debug_display_test(player, action_list):
-        print("")
-        print("")
-        game = TicTacToe()
-        for a in action_list:
-            game.advance(a)
-        game.display()
-        player.move(game)
-        game.display()
+
+def debug_display_ttt(player, action_list):
+    print("")
+    print("")
+    game = TicTacToe()
+    for a in action_list:
+        game.advance(a)
+    game.display()
+    # Do the optimal move. (No randomness).
+    player.move(game, print_info=True, randomness=False)
+    game.display()
