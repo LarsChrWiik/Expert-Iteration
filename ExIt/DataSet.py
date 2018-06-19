d20e0b19acd8a12a02ccf8385b2df8741c713392
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-19 22:04:59 +0100

    Removed old code and refactoring.

--- ExIt/DataSet.py
+++ ExIt/DataSet.py
@@ -9,40 +9,27 @@ class DataSet:
     def __init__(self):
         self.__clear()
 
-    def add_sample(self, state: BaseGame, action_index, r):
-        """ Add information to sample arrays. Array indexes refer to the same sample. """
+    def add_sample(self, state: BaseGame, action_index, v):
+        """ Add information to sample arrays. Array indexes refer to the same sample """
         pi_update = np.zeros(state.num_actions, dtype=float)
         pi_update[action_index] = 1
         turn, fv, pi = state.turn, state.get_feature_vector(state.turn), pi_update
-        self.__add_sample(fv=fv, pi=pi, r=r, turn=turn)
+        self.s_array.append(fv)
+        self.pi_array.append(pi)
+        self.v_array.append(v)
+        self.turn_array.append(turn)
+
+    def set_game_outcome_v_values(self, final_state: BaseGame):
+        """ Set all v values to the outcome of the game for each player.
+            This approach may lead to overfitting. """
+        for i, t in enumerate(self.turn_array):
+            self.v_array[i] = final_state.get_result(t).value
 
     def extract_data(self):
         """ Function that extract the data and clear the history """
-        data_tuple = self.s_array, self.pi_array, self.r_array
+        data_tuple = self.s_array, self.pi_array, self.v_array
         self.__clear()
         return data_tuple
 
-    # TODO: Not used. (Remove later).
-    def update_reward_soft(self, final_state: BaseGame):
-        """ Add relative reward to the improvement data when the game has finished """
-        play_len = len(self.turn_array)
-        for i, t in enumerate(self.turn_array):
-            f = final_state.get_reward(t)
-            v = self.r_array[i]
-            d = f - v
-            d = d * ((i+1) / (play_len+1))
-            self.r_array[i] = v + d
-
-    def update_reward_hard(self, final_state: BaseGame):
-        """ Set reward to the improvement data when the game has finished """
-        for i, t in enumerate(self.turn_array):
-            self.r_array[i] = final_state.get_reward(t)
-
-    def __add_sample(self, fv, pi, r, turn):
-        self.s_array.append(fv)
-        self.pi_array.append(pi)
-        self.r_array.append(r)
-        self.turn_array.append(turn)
-
     def __clear(self):
-        self.s_array, self.pi_array, self.r_array, self.turn_array = [[] for _ in range(4)]
+        self.s_array, self.pi_array, self.v_array, self.turn_array = [[] for _ in range(4)]
