0eddbe842c5df3c8c1a6d028c135aad9602c223c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-19 22:10:41 +0100

    .

--- main.py
+++ main.py
@@ -2,47 +2,60 @@
 
 from Games.TicTacToe import TicTacToe
 from Players.Players import *
-from Support.Debugger import TicTacToeDebugger
+from Support.Debugger import debug_display_win_moves
+from Matchmaking.Matchmaking import Matchmaking
+from Support.Plotter import plot_result
 import numpy as np
 np.set_printoptions(suppress=True)
 
 
-search_time = 0.05
+search_time = 0.2
 num_iteration = 10
 num_train_epoch = 10000
 num_matches = 1000
 
-debugger = TicTacToeDebugger()
-
 
 def main():
-    # Run Comparison with several iteration of self-play.
-    """
-    m = Matchmaking(game_class=TicTacToe,
-                    players=[NnMinimaxPlayer(), RandomPlayer()])
-
-    m.compare_ex_it(num_train_epoch=num_train_epoch,
-                    search_time=search_time,
-                    num_matches=num_matches,
-                    num_iteration=num_iteration,
-                    file_name="NnMinimaxPlayer_vs_RandomPlayer")
-    """
+    normal_test()
 
 
+def normal_test():
     # Run One iteration of self.play
     player = NnMinimaxPlayer()
-    #player = NnMctsPlayer()
+    # player = NnMctsPlayer()
     player.set_game(game_class=TicTacToe)
-    player.start_ex_it(num_iteration=10000,
-                       randomness=True,
-                       search_time=search_time)
-
+    player.start_ex_it(
+        num_iteration=1000,
+        randomness=True,
+        search_time=search_time
+    )
 
     # Display the agents calculations in predefined scenarios.
-    debugger.debug_display_win_moves(player)
+    debug_display_win_moves(player)
+
+
+def comparison():
+    # Run Comparison with several iteration of self-play.
+    Matchmaking(
+        game_class=TicTacToe,
+        players=[NnMinimaxPlayer(), RandomPlayer()]
+    ).compare_ex_it(
+        num_train_epoch=num_train_epoch,
+        search_time=search_time,
+        num_matches=num_matches,
+        num_iteration=num_iteration,
+        file_name="NnMinimaxPlayer_vs_RandomPlayer",
+        randomness=True
+    )
+
+
+def plot():
+    folder = '2018-06-18___02-20-59'
+    filename = "0"
+    plot_result(folder=folder, filename=filename)
 
 
 if __name__ == "__main__":
     main()
     #cProfile.run('main()')
-    #plot_test()
+    #plot()
