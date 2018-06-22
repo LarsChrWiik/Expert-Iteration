795c286d7ee50bf5f3ed2bd66a8bf04059c19ed3
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-22 20:30:07 +0100

    Added parameter, refactoring, added function to check Q values for actions.

--- Players/BasePlayers.py
+++ Players/BasePlayers.py
@@ -2,6 +2,8 @@
 from Games.GameLogic import BaseGame
 from ExIt.ExpertIteration import ExpertIteration
 from random import choice as rnd_choice
+from ExIt.Evaluator import get_reward_for_action
+from ExIt.ActionPolicy import e_greedy, get_action_index_exploit
 import numpy as np
 
 
@@ -25,12 +27,12 @@ class BasePlayer:
     def set_game(self, game_class):
         self.game_class = game_class
 
-    def move(self, state: BaseGame):
+    def move(self, state: BaseGame, randomness=False):
         raise NotImplementedError("Please Implement this method")
 
     @staticmethod
     def move_random(game: BaseGame):
-        legal_moves = game.get_possible_actions()
+        legal_moves = game.get_legal_moves()
         action_index = rnd_choice(legal_moves)
         game.advance(action_index=action_index)
 
@@ -42,6 +44,9 @@ class BaseExItPlayer(BasePlayer):
         super().__init__()
         self.__ex_it_algorithm = ex_it_algorithm
 
+    def get_apprentice_DO_NOT_USE(self):
+        return self.__ex_it_algorithm.apprentice
+
     def set_game(self, game_class):
         self.game_class = game_class
         self.__ex_it_algorithm.apprentice.init_model(input_fv_size=game_class().fv_size,
@@ -51,21 +56,36 @@ class BaseExItPlayer(BasePlayer):
         """ Move according to the apprentice (No expert) """
         fv = state.get_feature_vector(state.turn)
         pi = self.__ex_it_algorithm.apprentice.pred_prob(fv)
-        legal_moves = state.get_possible_actions()
-        if not randomness:
-            for i, x in enumerate(pi):
-                if i not in legal_moves:
-                    pi[i] = 0
-            action_index = self.__ex_it_algorithm.get_action_index_exploit(pi=pi)
-        else:
-            action_index = self.__ex_it_algorithm.get_action_index_explore(
-                pi=pi,
-                action_indexes=legal_moves
-            )
+        legal_moves = state.get_legal_moves()
+
+        # Remove PI values that are not legal moves.
+        pi_new = [x for i, x in enumerate(pi) if i in legal_moves]
+        pi = pi_new
+
+        action_index = e_greedy(pi=pi, legal_moves=legal_moves) if randomness \
+            else get_action_index_exploit(pi=pi, action_indexes=legal_moves)
 
         if print_info:
             self.print_info(state=state, action_index=action_index)
+            #self.spyros_function(state=state, pi=pi, legal_moves=legal_moves)
+
         state.advance(action_index=action_index)
+
+    def spyros_function(self, state, pi, legal_moves):
+        """ ---------- SPYROS ---------- START"""
+        Qs = [get_reward_for_action(state, a, self.__ex_it_algorithm.apprentice) for a in state.get_legal_moves()]
+        v_values, action_indexes, v = self.__ex_it_algorithm.expert.search(
+            state=state,
+            predictor=self.__ex_it_algorithm.apprentice,
+            search_time=10
+        )
+        print("pi = ", pi)
+        print("legal_moves = ", legal_moves)
+        print("Qs = ", Qs)
+        print("v_values = ", v_values)
+        print("action_indexes = ", action_indexes)
+        print("")
+        """ ---------- SPYROS ---------- END"""
 
     def start_ex_it(self, num_iteration, search_time: float):
         """ Starts Expert Iteration. NB: Time consuming process """
