2643c30d465cd312ba0cd9bc475202cf2fad830c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-23 20:19:55 +0100

    Compare Minimax with MCTS.

--- main.py
+++ main.py
@@ -2,30 +2,30 @@
 
 from Games.TicTacToe import TicTacToe
 from Players.Players import *
-from Support.Debugger import debug_display_win_moves
+from Support.Debugger import debug_display_win_moves, debug_display_general_moves
 from Matchmaking.Matchmaking import Matchmaking
 from Support.Plotter import plot_result
 import numpy as np
 np.set_printoptions(suppress=True)
 
 
-search_time = 0.2
-num_iteration = 10
-num_train_epoch = 10000
+search_time = 0.05
+num_iteration = 100
+num_train_epoch = 100
 num_matches = 1000
 
 
 def main():
-    normal_test()
+    comparison()
 
 
 def normal_test():
     # Run One iteration of self.play
-    player = NnMinimaxPlayer()
-    # player = NnMctsPlayer()
+    #player = NnMinimaxPlayer()
+    player = NnMctsPlayer()
     player.set_game(game_class=TicTacToe)
     player.start_ex_it(
-        num_iteration=1000,
+        num_iteration=1,
         search_time=search_time
     )
 
@@ -33,23 +33,56 @@ def normal_test():
     debug_display_win_moves(player)
 
 
+def test_easy_learning():
+    player = NnMinimaxPlayer()
+    s_array = [[1, 0, 0, 0, 0, 0, 0, 0, 0,   0, 1, 0, 0, 1, 0, 0, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 1, 0, 0, 0, 0, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 0, 1, 0, 0, 0, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 0, 0, 0, 1, 0, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 0, 0, 0, 0, 1, 0, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 0, 0, 0, 0, 0, 1, 0],
+               [0, 1, 0, 0, 1, 0, 0, 0, 0,   1, 0, 0, 0, 0, 0, 0, 0, 1]]
+
+    v_values = [[0], [1], [1], [1], [1], [0], [1]]
+
+    pi_values = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+    player.get_apprentice_DO_NOT_USE().init_model(
+        input_fv_size=len(s_array[0]),
+        pi_size=len(pi_values[0])
+    )
+
+    for _ in range(1000):
+        player.get_apprentice_DO_NOT_USE().train(
+            X=s_array,
+            Y_pi=pi_values,
+            Y_v=v_values
+        )
+    debug_display_general_moves(player)
+
+
 def comparison():
     # Run Comparison with several iteration of self-play.
     Matchmaking(
         game_class=TicTacToe,
-        players=[NnMinimaxPlayer(), RandomPlayer()]
+        players=[NnMctsPlayer(), NnMinimaxPlayer()]
     ).compare_ex_it(
         num_train_epoch=num_train_epoch,
         search_time=search_time,
         num_matches=num_matches,
         num_iteration=num_iteration,
-        file_name="NnMinimaxPlayer_vs_RandomPlayer",
         randomness=True
     )
 
 
 def plot():
-    folder = '2018-06-18___02-20-59'
+    folder = '2018-06-20___00-39-18'
     filename = "0"
     plot_result(folder=folder, filename=filename)
 
@@ -57,4 +90,3 @@ def plot():
 if __name__ == "__main__":
     main()
     #cProfile.run('main()')
-    #plot()
