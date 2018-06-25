68f1ca4af5d38f91b4550676bea499e8a7d1d6b8
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-25 23:19:18 +0100

    Refactoring.

--- ExIt/DataSet.py
+++ ExIt/DataSet.py
@@ -30,8 +30,8 @@ class DataSet:
         for i, t in enumerate(self.turn_array):
             self.v_array[i] = final_state.get_result(t).value
 
-    def extract_data(self):
-        """ Function that extract the data and clear the history """
+    def save_samples_in_memory(self):
+        """ Stores samples from the previous game and clears the history """
         new_data_tuple = [self.s_array, self.pi_array, self.v_array]
         if self.memory is None:
             self.memory = new_data_tuple
@@ -39,16 +39,18 @@ class DataSet:
             for i in range(len(self.memory)):
                 self.memory[i].extend(new_data_tuple[i])
             self.memory = [t[-max_experience_replay_memory_size:] for t in self.memory]
-
         self.__clear()
-        all_examples_indices = len(self.memory[0])
+
+    def get_sample_batch(self):
+        """ Chooses a random batch from the samples stored in memory """
+        sample_size = len(self.memory[0])
         mini_batch_indices = np.random.choice(
-            all_examples_indices,
-            size=min(batch_size, all_examples_indices),
+            sample_size,
+            size=min(batch_size, sample_size),
             replace=False
         )
-
         return list([np.array(k)[mini_batch_indices] for k in self.memory])
 
     def __clear(self):
+        """ Clears the history from previous game """
         self.s_array, self.pi_array, self.v_array, self.turn_array = [[] for _ in range(4)]
