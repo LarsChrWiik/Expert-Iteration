01f23c2e9dd2db3caaa342388596a3b58e926068
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-25 23:19:37 +0100

    Changes due to change in dataset.

--- ExIt/ExpertIteration.py
+++ ExIt/ExpertIteration.py
@@ -30,22 +30,28 @@ class ExpertIteration:
             state = game_class()
             X = state.get_feature_vector()
 
+            num_states = 0
             while not state.is_game_over():
                 self.ex_it_state(state=state, search_time=search_time)
+                num_states += 1
+
             self.games_played += 1
+
+            self.data_set.set_game_outcome_v_values(final_state=state)
 
             # TODO: Delete later.
             print("*** Iteration = " + str(self.games_played) + " ***")
 
-            #print("v_array = ", v_array)
+            print("v_array = ", self.data_set.v_array[-num_states:])
             print("     pi = ", self.apprentice.pred_prob(X=X))
-            #print(" pi_new = ", pi_array[0])
+            print(" pi_new = ", self.data_set.pi_array[-num_states])
             print("v_start = ", self.apprentice.pred_eval(X=X))
             print("")
             print("")
             print("")
 
-        s_array, pi_array, v_array = self.data_set.extract_data()
+        self.data_set.save_samples_in_memory()
+        s_array, pi_array, v_array = self.data_set.get_sample_batch()
         self.apprentice.train(X=s_array, Y_pi=pi_array, Y_v=v_array)
 
     def ex_it_state(self, state: BaseGame, search_time: float):
