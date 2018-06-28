c39160872b69abb02a63cd462a33ca93d5bb4531
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-29 01:14:11 +0200

    Best tuning.

--- ExIt/Apprentice/Nn.py
+++ ExIt/Apprentice/Nn.py
@@ -21,67 +21,72 @@ def custom_loss(y_true, y_pred, beta=0.001):
     return categorical_crossentropy(y_true, y_pred) - beta * entropy(y_pred)
 
 
+n_neurons = 256
+n_layers = 3
+dropout_rate = 0.1
+v_size = 1
+
+
 class Nn(BaseApprentice):
     """ Deep neural network implementation (Apprentice) """
 
     def __init__(self):
         self.model = None
         self.optimizer = None
+        self.policy_network = None
+        self.value_network = None
 
     def init_model(self, input_fv_size, pi_size):
         # Input layer.
-        x = Input(shape=(input_fv_size,))
+        input = Input(shape=(input_fv_size,))
 
-        # Hidden layer 1.
-        h1 = Dense(256, kernel_regularizer=l2())(x)
-        h1 = BatchNormalization()(h1)
-        h1 = Activation("elu")(h1)
-        h1 = Dropout(rate=0.1)(h1)
-
-        # Hidden layer 2
-        h2 = Dense(256, kernel_regularizer=l2())(h1)
-        h2 = BatchNormalization()(h2)
-        h2 = Activation("elu")(h2)
-        h2 = Dropout(rate=0.1)(h2)
+        # Hidden layers.
+        x = input
+        for _ in range(n_layers):
+            x = Dense(n_neurons, kernel_regularizer=l2(0.001))(x)
+            x = BatchNormalization()(x)
+            x = Activation("elu")(x)
+            #x = Dropout(rate=dropout_rate)(x)
 
         # Output layer pi = Action probability.
-        pi = Dense(pi_size, activation='linear', kernel_regularizer=l2())(h2)
-        #pi = BatchNormalization()(pi)
-        pi = Activation("softmax", name='pi_output')(pi)
+        p = Dense(pi_size, kernel_regularizer=l2(0.001))(x)
+        p = BatchNormalization()(p)
+        p = Activation("softmax", name='p_output')(p)
         # Output layer v = state evaluation
-        v = Dense(1, activation='linear', kernel_regularizer=l2())(h2)
-        #v = BatchNormalization()(v)
+        v = Dense(v_size, kernel_regularizer=l2(0.001))(x)
+        v = BatchNormalization()(v)
         v = Activation("tanh", name='v_output')(v)
 
-        model = Model(inputs=[x], outputs=[pi, v])
-        model.summary()
+        model = Model(inputs=[input], outputs=[p, v])
 
         optimizer = Adam()
         model.compile(
             optimizer=optimizer,
             loss={
-                'pi_output': categorical_crossentropy,
+                'p_output': categorical_crossentropy,
                 'v_output': mean_squared_error
-            },
-            loss_weights={
-                'pi_output': 1,
-                'v_output': 1
             }
         )
 
         self.optimizer = optimizer
         self.model = model
+        self.policy_network = Model(inputs=[input], outputs=[p])
+        self.value_network = Model(inputs=[input], outputs=[v])
 
     def train(self, X, Y_pi, Y_v):
-        self.model.fit(x=np.array(X), y=[np.array(Y_pi), np.array(Y_v)])
+        _, policy_loss, value_loss = self.model.train_on_batch(
+            x=np.array(X),
+            y=[np.array(Y_pi), np.array(Y_v)]
+        )
+        return policy_loss, value_loss
 
     def pred_eval(self, X):
         """ Return float representing state evaluation """
-        return self.model.predict(x=np.array([X]))[1][0][0]
+        return self.value_network.predict(x=np.array([X]))[0][0]
 
     def pred_prob(self, X):
         """ Return array of action probabilities """
-        return self.model.predict(x=np.array([X]))[0][0]
+        return self.policy_network.predict(x=np.array([X]))[0]
 
     def set_lr(self, new_lr):
         """ Set new learning rate """
