032b259f21c986cfff4a5a209c030f143d1b0e9b
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-29 01:17:43 +0200

    .

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ np.set_printoptions(suppress=True)
 
 
 search_time = 0.1
-num_iteration = 100
-num_train_epoch = 128
+num_iteration = 20
+num_train_epoch = 1000
 num_matches = 1000
+
+from numpy.random import dirichlet
 
 
 def main():
@@ -21,8 +23,8 @@ def main():
 
 def normal_test():
     # Run One iteration of self.play
-    #player = NnMinimaxPlayer()
-    player = NnMctsPlayer()
+    player = NnMinimaxPlayer()
+    #player = NnMctsPlayer()
     player.set_game(game_class=TicTacToe)
     player.start_ex_it(
         num_iteration=1,
@@ -37,7 +39,7 @@ def comparison():
     # Run Comparison with several iteration of self-play.
     Matchmaking(
         game_class=TicTacToe,
-        players=[NnMinimaxPlayer(), RandomPlayer()]
+        players=[NnMinimaxOneDepthPlayer(), RandomPlayer()]
     ).compare_ex_it(
         num_train_epoch=num_train_epoch,
         search_time=search_time,
