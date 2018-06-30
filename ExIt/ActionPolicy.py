d0f836cd69563557fd04675e36232df4bea5492f
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-06-30 15:13:07 +0200

    Parameter renaming and commenting.

--- ExIt/ActionPolicy.py
+++ ExIt/ActionPolicy.py
@@ -3,24 +3,24 @@ import numpy as np
 import random
 
 
-def get_action_index_exploit(pi, action_indexes):
+def get_action_index_exploit(values, legal_moves):
     """ EXPLOIT.
-        Assumes that PI has removed moves that are not legal.
-        Also assumes that the index of pi and action_indexes corresponds. """
-    return action_indexes[np.argmax(pi)]
+        Assumes that 'values' has removed moves that are not legal.
+        Also assumes that the index of 'values' and legal_moves corresponds. """
+    return legal_moves[np.argmax(values)]
 
 
-def get_action_index_explore(pi, action_indexes):
+def get_action_index_explore(values, legal_moves):
     """ EXPLORE.
-        Assumes that PI has removed moves that are not legal.
-        Also assumes that the index of pi and action_indexes corresponds. """
-    return np.random.choice(a=action_indexes, size=1, p=pi)[0]
+        Assumes that 'values' has removed moves that are not legal.
+        Also assumes that the index of 'values' and legal_moves corresponds. """
+    return np.random.choice(a=legal_moves, size=1, p=values)[0]
 
 
 def e_greedy(pi, legal_moves, e):
     """ Assumes that PI has removed moves that are not legal.
         Also assumes that the index of pi and action_indexes corresponds. """
-    best_action = get_action_index_exploit(pi=pi, action_indexes=legal_moves)
+    best_action = get_action_index_exploit(values=pi, legal_moves=legal_moves)
     if random.uniform(0, 1) < e:
         return best_action, random.choice(legal_moves)
     return best_action, best_action
