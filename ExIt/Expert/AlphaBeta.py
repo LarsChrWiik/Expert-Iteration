2e5826e56cc8c528786a2460578f6475169fb268
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-01 13:18:29 +0200

    Working AB player.

--- ExIt/Expert/AlphaBeta.py
+++ ExIt/Expert/AlphaBeta.py
@@ -8,35 +8,48 @@ from ExIt.Evaluator import zero_sum_2v2_evaluation
 
 def alpha_beta_search(state, alpha, beta, depth, predictor, original_turn, timer, is_root):
 
-    def max_value(state, alpha, beta, depth):
+    def max_value(state, alpha, beta, depth, is_root):
         val = float('-inf')
-        for a in state.get_legal_moves():
+        action_indexes = state.get_legal_moves()
+        v_values = [0 for _ in action_indexes]
+        for i, a in enumerate(action_indexes):
             c = state.copy()
             c.advance(a)
             val = max(
                 val,
                 alpha_beta_search(c, alpha, beta, depth - 1, predictor, original_turn, timer, False)
             )
+            v_values[i] = val
             if val >= beta:
                 return val
             alpha = max(alpha, val)
+        if is_root:
+            return v_values, action_indexes, val
         return val
 
-    def min_value(state, alpha, beta, depth):
+    def min_value(state, alpha, beta, depth, is_root):
         val = float('inf')
-        for a in state.get_legal_moves():
+        action_indexes = state.get_legal_moves()
+        v_values = [0 for _ in action_indexes]
+        for i, a in enumerate(action_indexes):
             c = state.copy()
             c.advance(a)
             val = min(
                 val,
                 alpha_beta_search(c, alpha, beta, depth - 1, predictor, original_turn, timer, False)
             )
+            v_values[i] = val
             if val <= alpha:
                 return val
             beta = min(beta, val)
+        if is_root:
+            return v_values, action_indexes, val
         return val
 
-    if state.is_game_over() or depth <= 0 or not timer.have_time_left():
+    # TODO: fix     or not timer.have_time_left()
+    # TODO: Make optinal fixed deapth.
+    # TODO: Maybe train more?
+    if state.is_game_over() or depth <= 0:
         return zero_sum_2v2_evaluation(
             state=state,
             original_turn=original_turn,
@@ -61,13 +74,12 @@ class AlphaBeta(BaseExpert):
         timer = Timer()
         timer.start_search_timer(search_time=search_time)
 
-        #best_action = None
-        depth = 0
-        val_last = float('-inf')
-        val = float('-inf')
+        depth = 1
+        v_values_last, action_indexes_last, val_last = [], [], float('-inf')
+        v_values, action_indexes, val = [], [], float('-inf')
         while timer.have_time_left():
-            val_last = val
-            val = alpha_beta_search(
+            v_values, action_indexes, val = v_values_last, action_indexes_last, val_last
+            v_values_last, action_indexes_last, val_last = alpha_beta_search(
                 state=state,
                 alpha=float('-inf'),
                 beta=float('inf'),
@@ -78,10 +90,6 @@ class AlphaBeta(BaseExpert):
                 is_root=True
             )
             depth += 1
-            print("val = ", val)
 
-        print("final val = ", val_last)
-
-        v_values, action_indexes = ...
-        return v_values, action_indexes, root_node.evaluation
+        return v_values, action_indexes, val
 
