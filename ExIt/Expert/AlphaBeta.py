1b2805f821c67345108278e8e3977ef403e111e2
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-01 12:51:02 +0200

    Added unfinished version of AB search.

---
+++ ExIt/Expert/AlphaBeta.py
@@ -0,0 +1,89 @@
+
+from ExIt.Expert.BaseExpert import BaseExpert
+from ExIt.Apprentice import BaseApprentice
+from Games.GameLogic import BaseGame
+from Support.Timer import Timer
+from ExIt.Evaluator import zero_sum_2v2_evaluation
+
+
+def alpha_beta_search(state, alpha, beta, depth, predictor, original_turn, timer):
+
+    def max_value(state, alpha, beta, depth):
+        val = float('-inf')
+        action_indexes = state.get_legal_moves()
+        values = [0 for _ in action_indexes]
+        for i, a in enumerate(action_indexes):
+            c = state.copy()
+            c.advance(a)
+
+            vs, ass, evals = max(
+                val,
+                alpha_beta_search(c, alpha, beta, depth - 1, predictor, original_turn, timer)
+            )
+            if val >= beta:
+                return val
+            alpha = max(alpha, val)
+        return val
+
+    def min_value(state, alpha, beta, depth):
+        val = float('inf')
+        for a in state.get_legal_moves():
+            c = state.copy()
+            c.advance(a)
+            val = min(
+                val,
+                alpha_beta_search(c, alpha, beta, depth - 1, predictor, original_turn, timer)
+            )
+            if val <= alpha:
+                return val
+            beta = min(beta, val)
+        return val
+
+    if state.is_game_over() or depth <= 0 or not timer.have_time_left():
+        return zero_sum_2v2_evaluation(
+            state=state,
+            original_turn=original_turn,
+            predictor=predictor
+        )
+
+    if state.turn == original_turn:
+        return max_value(state, alpha, beta, depth)
+    else:
+        return min_value(state, alpha, beta, depth)
+
+
+class AlphaBeta(BaseExpert):
+    """ This implementation is limited to Zero-sum,
+        two-player deterministic markov games """
+
+    def __init__(self):
+        self.timer = Timer()
+
+    def search(self, state: BaseGame, predictor: BaseApprentice, search_time: float):
+
+        timer = Timer()
+        timer.start_search_timer(search_time=search_time)
+
+        #best_action = None
+        depth = 0
+        val_last = float('-inf')
+        val = float('-inf')
+        while timer.have_time_left():
+            val_last = val
+            val = alpha_beta_search(
+                state=state,
+                alpha=float('-inf'),
+                beta=float('inf'),
+                depth=depth,
+                predictor=predictor,
+                original_turn=state.turn,
+                timer=timer
+            )
+            depth += 1
+            print("val = ", val)
+
+        print("final val = ", val_last)
+
+        v_values, action_indexes = root_node.get_v_actions_and_index()
+        return v_values, action_indexes, root_node.evaluation
+
