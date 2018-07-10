817a0ba712c14eb58f664dd8ee6f06c7773a5f7c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-11 00:42:22 +0100

    Added protortional function.

--- src/ExIt/ActionPolicy.py
+++ src/ExIt/ActionPolicy.py
@@ -3,24 +3,37 @@ import numpy as np
 import random
 
 
-def get_action_index_exploit(values, legal_moves):
+def exploit_action(values, legal_moves):
     """ EXPLOIT.
         Assumes that 'values' has removed moves that are not legal.
         Also assumes that the index of 'values' and legal_moves corresponds. """
     return legal_moves[np.argmax(values)]
 
 
-def get_action_index_explore(values, legal_moves):
+def explore_action(values, legal_moves):
     """ EXPLORE.
         Assumes that 'values' has removed moves that are not legal.
         Also assumes that the index of 'values' and legal_moves corresponds. """
     return np.random.choice(a=legal_moves, size=1, p=values)[0]
 
 
-def e_greedy(pi, legal_moves, e):
+def e_greedy(p, legal_moves, e):
     """ Assumes that PI has removed moves that are not legal.
         Also assumes that the index of pi and action_indexes corresponds. """
-    best_action = get_action_index_exploit(values=pi, legal_moves=legal_moves)
+    best_action = exploit_action(values=p, legal_moves=legal_moves)
     if random.uniform(0, 1) < e:
         return best_action, random.choice(legal_moves)
     return best_action, best_action
+
+
+def p_proportional(p, v, legal_moves):
+    """ Assumes that p indexes corresponds to action_indexes. """
+    best_action = exploit_action(values=v, legal_moves=legal_moves)
+
+    p[best_action] += 0.1
+    p = p[legal_moves]
+    p = p / p.sum()
+
+    action_taken = explore_action(p, legal_moves)
+
+    return best_action, action_taken
