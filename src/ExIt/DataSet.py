40906ce027d2fd5d85f531c919c5594c74ee1a63
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-11 00:40:45 +0100

    Added another type of dataset.

--- src/ExIt/DataSet.py
+++ src/ExIt/DataSet.py
@@ -8,7 +8,7 @@ batch_size = 1024
 max_memory_size = 50000
 
 
-class DataSet:
+class DataSetUniqueStates:
     """ Class that contains the data set logic for improving the Apprentice """
 
     def __init__(self):
@@ -38,3 +38,37 @@ class DataSet:
         Y_v = [self.memory[tuple(key)][1] for key in X_s]
 
         return X_s, Y_p, Y_v
+
+
+class DataSet:
+    """ Class that contains the data set logic for improving the Apprentice """
+
+    def __init__(self):
+        self.clear()
+        self.memory = None
+
+    def save_samples_in_memory(self, s_array, p_array, v_array):
+        """ Stores samples from the previous game and clears the history """
+        new_data_tuple = [s_array, p_array, v_array]
+        if self.memory is None:
+            self.memory = new_data_tuple
+        else:
+            for i in range(len(self.memory)):
+                self.memory[i].extend(new_data_tuple[i])
+            self.memory = [t[-max_memory_size:] for t in self.memory]
+        self.clear()
+
+    def get_sample_batch(self):
+        """ Chooses a random batch from the samples stored in memory """
+        sample_size = len(self.memory[0])
+        size = min(batch_size, sample_size)
+        mini_batch_indices = np.random.choice(
+            sample_size,
+            size=size,
+            replace=False
+        )
+        return list([np.array(k)[mini_batch_indices] for k in self.memory])
+
+    def clear(self):
+        """ Clears the history from previous game """
+        self.s_array, self.pi_array, self.v_array, self.turn_array = [[] for _ in range(4)]
