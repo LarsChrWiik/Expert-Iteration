1b7caa85eb94f7fb12d1cf2750a925ca25c39218
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-11 14:05:01 +0100

    Chnages parameter names, commenting and minor changes.

--- src/ExIt/ActionPolicy.py
+++ src/ExIt/ActionPolicy.py
@@ -7,33 +7,36 @@ def exploit_action(values, legal_moves):
     """ EXPLOIT.
         Assumes that 'values' has removed moves that are not legal.
         Also assumes that the index of 'values' and legal_moves corresponds. """
-    return legal_moves[np.argmax(values)]
+    a_off_policy = legal_moves[np.argmax(values)]
+    return a_off_policy
 
 
 def explore_action(values, legal_moves):
     """ EXPLORE.
         Assumes that 'values' has removed moves that are not legal.
         Also assumes that the index of 'values' and legal_moves corresponds. """
-    return np.random.choice(a=legal_moves, size=1, p=values)[0]
+    p = [v/sum(values) for v in values]
+    a_on_policy = np.random.choice(a=legal_moves, size=1, p=p)[0]
+    return a_on_policy
 
 
-def e_greedy(p, legal_moves, e):
+def e_greedy(pi, legal_moves, e):
     """ Assumes that PI has removed moves that are not legal.
         Also assumes that the index of pi and action_indexes corresponds. """
-    best_action = exploit_action(values=p, legal_moves=legal_moves)
+    best_action = exploit_action(values=pi, legal_moves=legal_moves)
     if random.uniform(0, 1) < e:
         return best_action, random.choice(legal_moves)
     return best_action, best_action
 
 
-def p_proportional(p, v, legal_moves):
-    """ Assumes that p indexes corresponds to action_indexes. """
-    best_action = exploit_action(values=v, legal_moves=legal_moves)
+def p_proportional(pi, vi, legal_moves):
+    """ Also assumes that the index of pi and action_indexes corresponds. """
+    a_off_policy = exploit_action(values=vi, legal_moves=legal_moves)
 
-    p[best_action] += 0.1
-    p = p[legal_moves]
-    p = p / p.sum()
+    pi[a_off_policy] += 0.1
+    pi = pi[legal_moves]
+    pi = pi / pi.sum()
 
-    action_taken = explore_action(p, legal_moves)
+    a_on_policy = explore_action(pi, legal_moves)
 
-    return best_action, action_taken
+    return a_off_policy, a_on_policy
