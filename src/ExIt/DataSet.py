721fd3e807ade937ec01c8632bdbed1bbe902166
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-12 18:23:50 +0100

    Reordering of classes.

--- src/ExIt/DataSet.py
+++ src/ExIt/DataSet.py
@@ -8,38 +8,6 @@ batch_size = 1024
 max_memory_size = 50000
 
 
-class DataSetUniqueStates:
-    """ Class that contains the data set logic for improving the Apprentice """
-
-    def __init__(self):
-        self.memory = {}
-
-    def save_samples_in_memory(self, s_array, p_array, v_array):
-        """ Stores samples from the previous game and clears the history """
-        for i, s in enumerate(s_array):
-            self.memory[tuple(s)] = tuple(p_array[i]), v_array[i]
-        while len(self.memory) > max_memory_size:
-            rnd_index = random.choice(list(self.memory.keys()))
-            self.memory.pop(rnd_index)
-
-    def get_sample_batch(self):
-        """ Chooses a random batch from the samples stored in memory """
-        states = list(self.memory.keys())
-        all_examples_indices = len(states)
-        mini_batch_indices = np.random.choice(
-            all_examples_indices,
-            size=min(batch_size, all_examples_indices),
-            replace=False
-        )
-
-        # Extract samples and generate inputs and targets.
-        X_s = np.array(states)[mini_batch_indices]
-        Y_p = [list(self.memory[tuple(key)][0]) for key in X_s]
-        Y_v = [self.memory[tuple(key)][1] for key in X_s]
-
-        return X_s, Y_p, Y_v
-
-
 class DataSet:
     """ Class that contains the data set logic for improving the Apprentice """
 
@@ -72,3 +40,41 @@ class DataSet:
     def clear(self):
         """ Clears the history from previous game """
         self.s_array, self.pi_array, self.v_array, self.turn_array = [[] for _ in range(4)]
+
+    def get_size(self):
+        return len(self.memory[0])
+
+
+class DataSetUniqueStates:
+    """ Class that contains the data set logic for improving the Apprentice """
+
+    def __init__(self):
+        self.memory = {}
+
+    def save_samples_in_memory(self, s_array, p_array, v_array):
+        """ Stores samples from the previous game and clears the history """
+        for i, s in enumerate(s_array):
+            self.memory[tuple(s)] = tuple(p_array[i]), v_array[i]
+        while len(self.memory) > max_memory_size:
+            rnd_index = random.choice(list(self.memory.keys()))
+            self.memory.pop(rnd_index)
+
+    def get_sample_batch(self):
+        """ Chooses a random batch from the samples stored in memory """
+        states = list(self.memory.keys())
+        all_examples_indices = len(states)
+        mini_batch_indices = np.random.choice(
+            all_examples_indices,
+            size=min(batch_size, all_examples_indices),
+            replace=False
+        )
+
+        # Extract samples and generate inputs and targets.
+        X_s = np.array(states)[mini_batch_indices]
+        Y_p = [list(self.memory[tuple(key)][0]) for key in X_s]
+        Y_v = [self.memory[tuple(key)][1] for key in X_s]
+
+        return X_s, Y_p, Y_v
+
+    def get_size(self):
+        return len(self.memory)
