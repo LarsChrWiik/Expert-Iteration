0da5e9f1fc9da0bdd06a3ac1ba519df283ac21ae
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-13 15:45:03 +0100

    Minor change.

--- src/main.py
+++ src/main.py
@@ -36,9 +36,11 @@ V[s]    = Predicted v value of state s.
 search_time = get_seconds(ms=50)
 num_matches = 1000
 
+num_versions = 3
+
 training_timer = TrainingTimer(
-    time_limit=get_seconds(s=100),
-    num_versions=10
+    time_limit=get_seconds(m=1),
+    num_versions=num_versions
 )
 
 
@@ -48,7 +50,8 @@ def main():
 
 def elo_tournament():
     start_elo_tournament(
-        players=[NnAlphaBetaPlayer(), NnMctsPlayer(), RandomPlayer()],
+        players_classes=[NnAlphaBetaPlayer, NnMctsPlayer, RandomPlayer],
+        num_versions=num_versions,
         game_class=TicTacToe,
         randomness=True
     )
@@ -77,7 +80,7 @@ def comparison_trained():
 
 def comparison_from_scratch():
     # Run Comparison with several iteration of self-play.
-    players = [NnMctsPlayer(), RandomPlayer()]
+    players = [NnAlphaBetaPlayer(), RandomPlayer()]
 
     compare_ex_it_from_scratch(
         game_class=TicTacToe,
