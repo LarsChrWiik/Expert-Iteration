6a1a12e405552584c12105856945e498176aedd9
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-14 01:12:07 +0100

    Added plot elo function.

--- src/Misc/Plotter.py
+++ src/Misc/Plotter.py
@@ -1,8 +1,38 @@
 
+from Misc.DiskHandler import read_ratings
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 import csv
 
 
+def plot_elo_ratings(game_class, versions):
+    tournament = read_ratings(game_class, versions)
+    colors = sns.color_palette("Set1", n_colors=len(tournament), desat=.5)
+
+    for i, dic in enumerate(tournament):
+        player_key = list(dic.keys())[0]
+        player_info = dic[player_key]
+        x = np.array([0] + player_info["elo"])
+        uncertainty = np.array([[0] + player_info["uncertainty+"], [0] + player_info["uncertainty-"]])
+        data = x + uncertainty
+        ax = sns.tsplot(data=data, color=colors[i], legend=True, condition=player_key, marker="o")
+
+    # Legend above the diagram.
+    ax.legend(
+        bbox_to_anchor=(0., 1.02, 1., .102),
+        loc=len(tournament),
+        ncol=1,
+        mode="expand",
+        borderaxespad=0.
+    )
+
+    ax.set(ylabel='Elo Rating', xlabel='Apprentice version')
+    plt.subplots_adjust(top=.75, bottom=0.15)
+    plt.show()
+
+
+# TODO: Re-implement.
 def plot_result(folder, filename):
     base_path = './Statistics' + "/" + folder
     path = base_path + "/" + filename + ".csv"
