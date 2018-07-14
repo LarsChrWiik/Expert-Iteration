9ec4ea1a6f55f8cd8606fb0749145b3ea501f1c7
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-14 01:07:23 +0100

    Added name.

--- src/ExIt/Expert/BaseExpert.py
+++ src/ExIt/Expert/BaseExpert.py
@@ -6,6 +6,9 @@ from Games.GameLogic import BaseGame
 class BaseExpert:
     """ Class for policy improvement logic """
 
+    def __init__(self):
+        self.__name__ = type(self).__name__
+
     def search(self, state: BaseGame, predictor: BaseApprentice, search_time, use_exploration_policy):
         """ Do policy improvement for a given state.
         :return: action_index """
