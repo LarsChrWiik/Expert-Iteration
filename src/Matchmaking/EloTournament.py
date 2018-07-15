e505939c5165aaf96876e6a026fe7594ef2474b0
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-15 14:35:33 +0100

    Saved more information to elo meta file.

--- src/Matchmaking/EloTournament.py
+++ src/Matchmaking/EloTournament.py
@@ -6,15 +6,17 @@ from Misc.DiskHandler import create_elo_folders, save_game_to_pgn, \
 from tqdm import tqdm, trange
 
 
-def start_elo_tournament(game_class, players_classes, num_versions, iterations=5, randomness=True):
+def start_elo_tournament(game_class, players_classes, num_versions, iterations=100, randomness=True):
     """ Match players in a tournament and writes matches to PGN files """
 
     base_path = create_elo_folders(game_class)
-    create_elo_meta_file(base_path, game_class, players_classes, iterations, randomness)
 
     players = load_trained_models(game_class, players_classes, range(num_versions))
-
     match_permutations = get_all_match_permutations(players)
+
+    create_elo_meta_file(base_path, game_class, players_classes, iterations, num_versions,
+                         match_permutations, randomness)
+
     # Match players with all permutations 'num_matches' times.
     progress_bar = tqdm(range(iterations * len(match_permutations)))
     progress_bar.set_description("Tournament")
