4c3303799f5b43befb347a4b7d0af48eacda8df6
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-16 16:31:17 +0100

    Number of matches is now a parameter and is unrelated to number of players.

--- src/Matchmaking/EloTournament.py
+++ src/Matchmaking/EloTournament.py
@@ -4,38 +4,38 @@ from Matchmaking.GameHandler import GameHandler
 from Misc.DiskHandler import create_elo_folders, save_game_to_pgn, \
     load_trained_models, create_elo_meta_file
 from tqdm import tqdm, trange
+import random
 
 
-def start_elo_tournament(game_class, raw_players, num_versions, iterations=100, randomness=True):
+def start_elo_tournament(game_class, raw_players, num_versions, num_matches, randomness=True):
     """ Match players in a tournament and writes matches to PGN files """
 
     base_path = create_elo_folders(game_class)
 
     players = load_trained_models(game_class, raw_players, range(num_versions))
-    match_permutations = get_all_match_permutations(players)
+    player_indexes = range(len(players))
 
-    create_elo_meta_file(base_path, game_class, raw_players, iterations, num_versions,
-                         match_permutations, randomness)
+    create_elo_meta_file(base_path, game_class, raw_players, num_matches, num_versions, randomness)
 
     # Match players with all permutations 'num_matches' times.
-    progress_bar = tqdm(range(iterations * len(match_permutations)))
+    progress_bar = tqdm(range(num_matches))
     progress_bar.set_description("Tournament")
-    for _ in range(iterations):
-        for i1, i2 in match_permutations:
+    matches_played = 0
+    while matches_played < num_matches:
+        for i1, p in enumerate(players):
+            # Get index of any other player.
+            i2 = i1
+            while i2 == i1:
+                i2 = random.choice(player_indexes)
+
+            # Match the players.
             p1, p2 = players[i1], players[i2]
             game_handler = GameHandler(game_class, [p1, p2], randomness)
             game_handler.play_game_until_finish()
             save_game_to_pgn(base_path, game_handler, p1, p2)
             progress_bar.update(1)
+            matches_played += 1
+            if matches_played >= num_matches:
+                break
+
     progress_bar.close()
-
-
-def get_all_match_permutations(players):
-    """ Generate a list of 2-tuples of all match permutations between players.
-        Total of len(players) * (len(players)-1) tuples. """
-    match_permutations = []
-    for i, p1 in enumerate(players):
-        for j, p2 in enumerate(players):
-            if p1 is not p2:
-                match_permutations.append((i, j))
-    return match_permutations
