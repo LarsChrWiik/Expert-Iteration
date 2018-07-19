91d6324da8fefaa61ec8c8f7392d2f53ab1ada53
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-19 22:52:00 +0100

    Now trains the same version for all players. This will minimize optimization differences between training players.

--- Misc/Training.py
+++ Misc/Training.py
@@ -7,25 +7,18 @@ def self_play_and_store_versions(game_class, players, search_time, training_time
     """ This process will produce several versions of an
         ExIt algorithm during self-play training """
 
+    player_and_path = []
     for p in players:
         if not isinstance(p, BaseExItPlayer):
             continue
         p.set_game(game_class)
-        # Create folders and meta file and get get base path.
         base_path = create_training_folders(game_class, p)
         create_training_meta_file(base_path, p, search_time, training_timer)
+        player_and_path.append((p, base_path))
 
-        # Start self-play iterations.
-        for i in range(training_timer.num_versions):
+    for i in range(training_timer.num_versions):
+        for p, path in player_and_path:
             # Self-train.
-            p.start_ex_it(
-                training_timer=training_timer,
-                search_time=search_time,
-                verbose=True
-            )
+            p.start_ex_it(training_timer, search_time, verbose=True)
             # Save model.
-            save_model(
-                model=p.ex_it_algorithm.apprentice.model,
-                base_path=base_path,
-                iteration=str(i+1)
-            )
+            save_model(p.ex_it_algorithm.apprentice.model, path, version=str(i + 1))
