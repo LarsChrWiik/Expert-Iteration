83bbc27051c79fecaf65e7a3f03245bba2683260
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-27 14:17:19 +0100

    X axis is now time rather that version.

--- Misc/Plotter.py
+++ Misc/Plotter.py
@@ -1,12 +1,12 @@
 
-from Misc.DiskHandler import read_ratings, get_comparison_base_path
+from Misc.DiskHandler import read_ratings, get_comparison_base_path, load_elo_version_time
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import csv
 
 
-def add_y_margins_inside(ax, num_versions, y_scale):
+def add_y_margins_inside(ax, max_time, tournament_count, y_scale):
     bottom = ax.get_ylim()[0]
     top = ax.get_ylim()[1]
 
@@ -14,12 +14,26 @@ def add_y_margins_inside(ax, num_versions, y_scale):
     y_difference = y * y_scale
 
     ax.set_ylim(bottom=bottom-y_difference, top=top+y_difference)
-    ax.set_xlim(0, num_versions + 1)
+    ax.set_xlim(0, max_time + (max_time / (tournament_count + 1)))
 
 
 def plot_elo_ratings(game_class, num_versions):
     tournament = read_ratings(game_class)
+    version_times = load_elo_version_time(game_class)
     colors = sns.color_palette("Set1", n_colors=len(tournament), desat=.5)
+
+    max_time = max([version_times[p] * num_versions for p, vt in version_times.items()])
+    # Decide unit type.
+    unit_label = "Seconds"
+    unit_division = 1
+    if max_time >= 60:
+        unit_division *= 60
+        max_time = max_time / 60
+        unit_label = "Minutes"
+    if max_time >= 60:
+        unit_division *= 60
+        max_time = max_time / 60
+        unit_label = "Hours"
 
     ax = None
     for i, dic in enumerate(tournament):
@@ -30,20 +44,19 @@ def plot_elo_ratings(game_class, num_versions):
         data = x + uncertainty
         if player_key.startswith("ExIt"):
             # ExIt players.
-            ax = sns.tsplot(data=data, time=range(1, num_versions+1), color=colors[i],
+            time = [(i+1)*version_times[player_key] / unit_division for i in range(num_versions)]
+            ax = sns.tsplot(data=data, time=time, color=colors[i],
                             condition=player_key, marker="o", linestyle="-",
                             ax=ax)
         else:
             # Non-ExIt players.
             data = [[a, a] for a in data.tolist()]
-            ax = sns.tsplot(data=data, time=[0, num_versions+1], color=colors[i], linewidth=2,
+            ax = sns.tsplot(data=data, time=[-9999, 9999], color=colors[i], linewidth=2,
                             condition=player_key, marker="", linestyle="--", ax=ax)
 
-    ax.set_xlim(0, num_versions)
-
-    ax.set(ylabel='Elo Rating', xlabel='Apprentice version')
-    plt.subplots_adjust(top=0.9 - len(tournament)*0.04, bottom=0.15)
-    add_y_margins_inside(ax, num_versions, y_scale=0.1)
+    ax.set(ylabel='Elo Rating', xlabel='Training ' + unit_label)
+    plt.subplots_adjust(top=0.9 - len(tournament) * 0.04, bottom=0.15)
+    add_y_margins_inside(ax, max_time, len(tournament), y_scale=0.1)
 
     # Legend above the diagram.
     ax.legend(
