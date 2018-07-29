b0af99a007ba0ed4e8a422d2b6fc328d639cbab0
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-07-29 21:05:07 +0100

    Added Memory that forgets ans grows at the same time.

--- ExIt/Memory.py
+++ ExIt/Memory.py
@@ -4,12 +4,15 @@ import numpy as np
 import random
 
 
-batch_size = 1024
-max_memory_size = 50000
+global_batch_size = 1024
+global_max_memory_size = 50000
 
 
 class MemoryList:
-    """ Class that contains the data set logic for improving the Apprentice """
+    """ Class that contains the memory logic for improving the Apprentice """
+
+    batch_size = global_batch_size
+    max_memory_size = global_max_memory_size
 
     def __init__(self):
         self.clear()
@@ -23,13 +26,13 @@ class MemoryList:
         else:
             for i in range(len(self.memory)):
                 self.memory[i].extend(new_data_tuple[i])
-            self.memory = [t[-max_memory_size:] for t in self.memory]
+            self.memory = [t[-self.max_memory_size:] for t in self.memory]
         self.clear()
 
     def get_batch(self):
         """ Chooses a random batch from the samples stored in memory """
         sample_size = len(self.memory[0])
-        size = min(batch_size, sample_size)
+        size = min(self.batch_size, sample_size)
         mini_batch_indices = np.random.choice(
             sample_size,
             size=size,
@@ -46,7 +49,10 @@ class MemoryList:
 
 
 class MemorySet:
-    """ Class that contains the data set logic for improving the Apprentice """
+    """ Class that contains the memory logic for improving the Apprentice """
+
+    batch_size = global_batch_size
+    max_memory_size = global_max_memory_size
 
     def __init__(self):
         self.memory = {}
@@ -55,7 +61,7 @@ class MemorySet:
         """ Stores samples from the previous game and clears the history """
         for i, s in enumerate(s_array):
             self.memory[tuple(s)] = tuple(p_array[i]), v_array[i]
-        while len(self.memory) > max_memory_size:
+        while len(self.memory) > self.max_memory_size:
             rnd_index = random.choice(list(self.memory.keys()))
             self.memory.pop(rnd_index)
 
@@ -65,7 +71,7 @@ class MemorySet:
         all_examples_indices = len(states)
         mini_batch_indices = np.random.choice(
             all_examples_indices,
-            size=min(batch_size, all_examples_indices),
+            size=min(self.batch_size, all_examples_indices),
             replace=False
         )
 
@@ -78,3 +84,49 @@ class MemorySet:
 
     def get_size(self):
         return len(self.memory)
+
+
+class MemoryListGrowing:
+    """ Class that contains the memory logic for improving the Apprentice """
+
+    batch_size = global_batch_size
+    # Memory size that is growing. Initially set to the same size as the batch.
+    max_memory_size = batch_size
+    absolute_max_memory_size = global_max_memory_size
+
+    def __init__(self):
+        self.clear()
+        self.memory = None
+
+    def save(self, s_array, p_array, v_array):
+        """ Stores samples from the previous game and clears the history """
+        new_data_tuple = [s_array, p_array, v_array]
+        if self.memory is None:
+            self.memory = new_data_tuple
+        else:
+            for i in range(len(self.memory)):
+                self.memory[i].extend(new_data_tuple[i])
+            if len(self.memory[0]) > self.max_memory_size:
+                self.memory = [t[len(s_array):] for t in self.memory]
+                if self.max_memory_size < self.absolute_max_memory_size:
+                    self.max_memory_size += len(s_array)
+            self.memory = [t[-self.absolute_max_memory_size:] for t in self.memory]
+        self.clear()
+
+    def get_batch(self):
+        """ Chooses a random batch from the samples stored in memory """
+        sample_size = len(self.memory[0])
+        size = min(self.batch_size, sample_size)
+        mini_batch_indices = np.random.choice(
+            sample_size,
+            size=size,
+            replace=False
+        )
+        return list([np.array(k)[mini_batch_indices] for k in self.memory])
+
+    def clear(self):
+        """ Clears the history from previous game """
+        self.s_array, self.pi_array, self.v_array, self.turn_array = [[] for _ in range(4)]
+
+    def get_size(self):
+        return len(self.memory[0])
