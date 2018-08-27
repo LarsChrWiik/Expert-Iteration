0ebc18e023d13a5a9db75ce0f05a61c24f786516
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-08-27 22:42:43 +0100

    Cleanup.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-
 from Games.TicTacToe import TicTacToe
 from Games.ConnectFour import ConnectFour
 from Games.ConnectSix import ConnectSix
@@ -15,34 +14,13 @@ import numpy as np
 np.set_printoptions(suppress=True)
 
 
-"""
-***** Variable information in the project *****
-s   = state
-a   = action
-fv  = feature vector
-v   = predicted value of state
-vi  = list of values
-pi  = predicted action probability
-lm  = legal moves
-t = turn
-
-Q[s][a] = Expected Q values when taking action a from state s. 
-N[s]    = Number of times state s was visited. 
-P[s][a] = Predicted P values when taking action a from state s.
-V[s]    = Predicted v value of state s.
-***********************************************
-"""
-
-
-# ********** Run info START **********
-
 # Game.
 game_class = ConnectFour()
 
 # Players to compare.
 players = [
+    NnMinimaxPlayer(use_ab=True),
     NnMctsPlayer(),
-    NnMctsPlayer(growing_search=True),
     RandomPlayer(),
     BruteForcePlayer(depth=1),
     BruteForcePlayer(depth=2)
@@ -63,79 +41,21 @@ num_elo_matches = 10000
 # Chance of random action when advancing.
 match_randomness = 0.1
 
-# ********** Run info END **********
-
-
-
 
 def main():
-    #pipeline()
-    #plot_elo()
-    #comparison_trained()
-    comparison_from_scratch()
-    #test_play()
-
-
-def pipeline():
-    # Train.
-    #self_play_and_store_versions(game_class, players, search_time, training_timer)
-    # Tournament.
+    # This will store trained versions of players in the Trained_models folder.
+    self_play_and_store_versions(game_class, players, search_time, training_timer)
+    # This will generate a PGN file in Elo folder.
     start_elo_tournament(game_class, players, num_versions, num_elo_matches, match_randomness)
+    # Plot elo scores from rating.txt file in Elo folder after using Bayesian Elo.
+    #plot_elo(game_class, num_versions)
+    # Play against a player (either static or ExIt player with specified version).
+    #play_player(game_class, BruteForcePlayer(depth=2), search_time=None, version=None)
 
 
-def plot_elo():
+def plot_elo(game_class, num_versions):
     from Misc.Plotter import plot_elo_ratings
     plot_elo_ratings(game_class, num_versions)
-
-
-
-# TODO: Remove everything below later. (Used for testing).
-
-
-def test_play():
-    play_player(
-        game_class=game_class,
-        player=BruteForcePlayer(depth=2),
-        search_time=None,
-        version=None
-    )
-
-
-def train_and_store():
-    players = [NnMinimaxPlayer(use_ab=True), RandomPlayer()]
-    self_play_and_store_versions(
-        TicTacToe,
-        players,
-        search_time,
-        training_timer
-    )
-
-
-def comparison_from_scratch():
-    from Matchmaking.Comparison1v1 import compare_ex_it_from_scratch
-    # Run Comparison with several iteration of self-play.
-    players = [NnMctsPlayer(), RandomPlayer()]
-    compare_ex_it_from_scratch(
-        game_class=game_class,
-        players=players,
-        search_time=search_time,
-        num_matches=100,
-        training_timer=training_timer,
-        randomness=match_randomness # <---------------------------------- Remember!
-    )
-
-
-def comparison_trained():
-    from Matchmaking.Comparison1v1 import compare_ex_it_trained
-    players = [NnMinimaxPlayer(use_ab=True), BruteForcePlayer(depth=2)]
-    versions = [23]#range(20)
-    compare_ex_it_trained(
-        game_class=game_class,
-        raw_players=players,
-        num_matches=100,
-        randomness=match_randomness,  # <---------------------------------- Remember!
-        versions=versions
-    )
 
 
 if __name__ == "__main__":
