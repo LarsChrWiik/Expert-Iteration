4fd59c33684d1554962ac10f80bcb1669852390d
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-08-27 22:50:25 +0100

    Added comments.

--- ExIt/Expert/BaseExpert.py
+++ ExIt/Expert/BaseExpert.py
@@ -4,12 +4,12 @@ from Games.GameLogic import BaseGame
 
 
 class BaseExpert:
-    """ Class for policy improvement logic """
+    """ Class for the tree search algorithm used for policy improvement """
 
     def __init__(self):
         self.__name__ = type(self).__name__
 
     def search(self, state: BaseGame, predictor: BaseApprentice, search_time, use_exploration_policy):
         """ Do policy improvement for a given state.
-        :return: action_index """
+        :return: a_explore, a_optimal, soft-z """
         raise NotImplementedError("Please Implement this method")
